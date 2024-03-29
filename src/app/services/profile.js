acf1b46ff0aa546a23ec31bc91008f461e9e9796
Fede Villarreal <fedevillarreal94@gmail.com>
2024-03-29 20:42:47 -0300

    fix: Fixed code to update profile picture without app reload

--- src/app/services/profile.js
+++ src/app/services/profile.js
@@ -3,16 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const profileApi = createApi({
     reducerPath:"profileApi",
     baseQuery:fetchBaseQuery({baseUrl:"https://e-commerce-app-villarreal-default-rtdb.firebaseio.com"}),
+    tagTypes:['userImage'],
     endpoints:(builder)=>({
         putImage: builder.mutation({
             query:({image,localId})=>({
                 url:`/profile/${localId}.json`,
                 method:"PUT",
                 body:{image}
-            })
+            }),
+            invalidatesTags:['userImage']
         }),
         getImage: builder.query({
-            query:(localId) => `/profile/${localId}.json`
+            query:(localId) => `/profile/${localId}.json`,
+            providesTags:['userImage']
         })
     })
 })
